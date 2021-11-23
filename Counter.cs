c448a0fe1b05910f84dd3fc90062f50d22e1f9f1
faenerisis <67673597+faenerisis@users.noreply.github.com>
2021-11-23 09:56:09 +0300

    counter.cs
    It's a basic char counter program on c#.

---
+++ Counter.cs
@@ -0,0 +1,193 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String str1 = Console.ReadLine();
+            str1 = str1.Replace('A', 'a').Replace('B', 'b').Replace('C', 'c').Replace('D', 'd').Replace('E', 'e').Replace('F', 'f').Replace('G', 'g').Replace('H', 'h')
+                .Replace('I', 'i').Replace('J', 'j').Replace('K', 'k').Replace('L', 'l').Replace('M', 'm').Replace('N', 'n').Replace('O', 'o').Replace('P', 'p').Replace('R', 'r')
+                .Replace('S', 's').Replace('T', 't').Replace('U', 'u').Replace('V', 'v').Replace('Y', 'y').Replace('Z', 'z').Replace('X', 'x')
+                .Replace('Q', 'q').Replace('W', 'w');
+            if (str1.Contains('a'))
+            {
+                int include = str1.Split('a').Length - 1;
+                Console.WriteLine(include + " times a.");
+            }
+            if (str1.Contains('b') || str1.Contains('B'))
+            {
+                int include = str1.Split('b').Length - 1;
+                Console.WriteLine(include + " times b.");
+            }
+            if (str1.Contains('c'))
+            {
+                int include = str1.Split('c').Length - 1;
+                Console.WriteLine(include + " times c.");
+            }
+            if (str1.Contains('d'))
+            {
+                int include = str1.Split('d').Length - 1;
+                Console.WriteLine(include + " times d.");
+            }
+            if (str1.Contains('f'))
+            {
+                int include = str1.Split('f').Length - 1;
+                Console.WriteLine(include + " times f.");
+            }
+            if (str1.Contains('g'))
+            {
+                int include = str1.Split('g').Length - 1;
+                Console.WriteLine(include + " times g.");
+            }
+            if (str1.Contains('h'))
+            {
+                int include = str1.Split('h').Length - 1;
+                Console.WriteLine(include + " times h.");
+            }
+            if (str1.Contains('i'))
+            {
+                int include = str1.Split('i').Length - 1;
+                Console.WriteLine(include + " times i.");
+            }
+            if (str1.Contains('j'))
+            {
+                int include = str1.Split('j').Length - 1;
+                Console.WriteLine(include + " times j.");
+            }
+            if (str1.Contains('k'))
+            {
+                int include = str1.Split('k').Length - 1;
+                Console.WriteLine(include + " times k.");
+            }
+            if (str1.Contains('l'))
+            {
+                int include = str1.Split('l').Length - 1;
+                Console.WriteLine(include + " times l.");
+            }
+            if (str1.Contains('m'))
+            {
+                int include = str1.Split('m').Length - 1;
+                Console.WriteLine(include + " times m.");
+            }
+            if (str1.Contains('n'))
+            {
+                int include = str1.Split('n').Length - 1;
+                Console.WriteLine(include + " times n.");
+            }
+            if (str1.Contains('o'))
+            {
+                int include = str1.Split('o').Length - 1;
+                Console.WriteLine(include + " times o.");
+            }
+            if (str1.Contains('p'))
+            {
+                int include = str1.Split('p').Length - 1;
+                Console.WriteLine(include + " times p.");
+            }
+            if (str1.Contains('r'))
+            {
+                int include = str1.Split('r').Length - 1;
+                Console.WriteLine(include + " times r.");
+            }
+            if (str1.Contains('s'))
+            {
+                int include = str1.Split('s').Length - 1;
+                Console.WriteLine(include + " times s.");
+            }
+            if (str1.Contains('t'))
+            {
+                int include = str1.Split('t').Length - 1;
+                Console.WriteLine(include + " times t.");
+            }
+            if (str1.Contains('u'))
+            {
+                int include = str1.Split('u').Length - 1;
+                Console.WriteLine(include + " times u.");
+            }
+            if (str1.Contains('v'))
+            {
+                int include = str1.Split('v').Length - 1;
+                Console.WriteLine(include + " times v.");
+            }
+            if (str1.Contains('y'))
+            {
+                int include = str1.Split('y').Length - 1;
+                Console.WriteLine(include + " times y.");
+            }
+            if (str1.Contains('z'))
+            {
+                int include = str1.Split('z').Length - 1;
+                Console.WriteLine(include + " times z.");
+            }
+            if (str1.Contains('x'))
+            {
+                int include = str1.Split('x').Length - 1;
+                Console.WriteLine(include + " times x.");
+            }
+            if (str1.Contains('q'))
+            {
+                int include = str1.Split('q').Length - 1;
+                Console.WriteLine(include + " times q.");
+            }
+
+            if (str1.Contains('w'))
+            {
+                int include = str1.Split('w').Length - 1;
+                Console.WriteLine(include + " times w.");
+            }
+
+            if (str1.Contains('1'))
+            {
+                int include = str1.Split('1').Length - 1;
+                Console.WriteLine(include + " times 1.");
+            }
+            if (str1.Contains('2'))
+            {
+                int include = str1.Split('2').Length - 1;
+                Console.WriteLine(include + " times 2.");
+            }
+            if (str1.Contains('3'))
+            {
+                int include = str1.Split('3').Length - 1;
+                Console.WriteLine(include + " times 3.");
+            }
+            if (str1.Contains('4'))
+            {
+                int include = str1.Split('4').Length - 1;
+                Console.WriteLine(include + " times 4.");
+            }
+            if (str1.Contains('5'))
+            {
+                int include = str1.Split('5').Length - 1;
+                Console.WriteLine(include + " times 5.");
+            }
+            if (str1.Contains('6'))
+            {
+                int include = str1.Split('6').Length - 1;
+                Console.WriteLine(include + " times 6.");
+            }
+            if (str1.Contains('7'))
+            {
+                int include = str1.Split('7').Length - 1;
+                Console.WriteLine(include + " times 7.");
+            }
+            if (str1.Contains('8'))
+            {
+                int include = str1.Split('8').Length - 1;
+                Console.WriteLine(include + " times 8.");
+            }
+            if (str1.Contains('9'))
+            {
+                int include = str1.Split('0').Length - 1;
+                Console.WriteLine(include + " times 0.");
+            }
+            if (str1.Contains('0'))
+            {
+                int include = str1.Split('0').Length - 1;
+                Console.WriteLine(include + " times 0.");
+            }
+        }
+    }
+}
